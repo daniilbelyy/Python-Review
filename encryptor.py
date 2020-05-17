89af354595494e89f46ac9be94b4483690b93433
root <root@DESKTOP-QF03033.localdomain>
2020-05-17 16:43:54 +0300

    2fix

--- encryptor.py
+++ encryptor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import argparse
+import string
 
 def statistic(text, alphabet):
     alphabet_size = len(alphabet)
@@ -8,9 +9,9 @@ def statistic(text, alphabet):
     summ = 0
     for i in range(alphabet_size):
         stats.append(0)
-    for i in range(len(text)):
-        if text[i].lower() in alphabet:
-            stats[alphabet.index(text[i].lower())] += 1
+    for i in text:
+        if i.lower() in alphabet:
+            stats[alphabet.index(i.lower())] += 1
             summ += 1
     for i in range(alphabet_size):
         stats[i] /= summ
@@ -30,28 +31,49 @@ def put_into_file(given_file, text):
 
 def all_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('type')
-    parser.add_argument('--cipher')
-    parser.add_argument('--key')
-    parser.add_argument('--input-file')
-    parser.add_argument('--output-file')
-    parser.add_argument('--text-file')
-    parser.add_argument('--model-file')
-    return parser.parse_args(sys.argv[1:])
+    subparsers = parser.add_subparsers()
+
+    parser_encode = subparsers.add_parser('encode')
+    parser_encode.set_defaults(mode = 'encode')
+    parser_encode.add_argument('--cipher')
+    parser_encode.add_argument('--key')
+    parser_encode.add_argument('--input-file')
+    parser_encode.add_argument('--output-file')
+
+    parser_decode = subparsers.add_parser('decode')
+    parser_decode.set_defaults(mode = 'decode')
+    parser_decode.add_argument('--cipher')
+    parser_decode.add_argument('--key')
+    parser_decode.add_argument('--input-file')
+    parser_decode.add_argument('--output-file')
+
+    parser_hack = subparsers.add_parser('hack')
+    parser_hack.set_defaults(mode = 'hack')
+    parser_hack.add_argument('--cipher')
+    parser_hack.add_argument('--input-file')
+    parser_hack.add_argument('--output-file')
+    parser_hack.add_argument('--model-file')
+
+    parser_train = subparsers.add_parser('train')
+    parser_train.set_defaults(mode = 'train')
+    parser_train.add_argument('--text-file')
+    parser_train.add_argument('--output-file')
+
+    return parser.parse_args()
 
 def caesar(text, alphabet, key):
     alphabet_size = len(alphabet)
     key = key % alphabet_size
     new_text = list()
-    for i in range(len(text)):
-        if text[i] in alphabet:
-            letter_number = alphabet.index(text[i])
+    for i in text:
+        if i in alphabet:
+            letter_number = alphabet.index(i)
             letter_number += key
             if letter_number >= alphabet_size:
                 letter_number -= alphabet_size
             new_text.append(alphabet[letter_number])
         else:
-            new_text.append(text[i])
+            new_text.append(i)
     return ''.join(new_text)
 
 def vigenere(text, alphabet, key):
@@ -72,64 +94,42 @@ def vigenere(text, alphabet, key):
             new_text.append(text[i])
     return ''.join(new_text)
 
+def reversed_key(alphabet, key):
+    reversed_key = list()
+    for i in range (len(key)):
+        reversed_key.append(alphabet[(len(alphabet) - alphabet.index(key[i])) % len(alphabet)])
+    return ''.join(reversed_key)
 
-alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alphabet_lower = 'abcdefghijklmnopqrstuvwxyz' 
+
+alphabet = string.ascii_letters
+alphabet_lower = string.ascii_lowercase
 arguments = all_args()
-if arguments.type == 'encode':
-    if arguments.cipher == 'caesar':
-        if arguments.input_file != None:
-            text = take_out_of_file(arguments.input_file)
-        else:
-            text = str(input())
+if arguments.mode == 'encode' or arguments.mode == 'decode':
+    if arguments.input_file != None:
+        text = take_out_of_file(arguments.input_file)
+    else:
+        text = str(input())
+    if arguments.cipher == 'caesar' and arguments.mode == 'encode':
         new_text = caesar(text, alphabet, int(arguments.key))
-        if arguments.output_file != None:
-            put_into_file(arguments.output_file, new_text)
-        else:
-            print(new_text)
-    elif arguments.cipher == 'vigenere':
-        if arguments.input_file != None:
-            text = take_out_of_file(arguments.input_file)
-        else:
-            text = str(input())
-        new_text = vigenere(text, alphabet, arguments.key)
-        if arguments.output_file != None:
-            put_into_file(arguments.output_file, new_text)
-        else:
-            print(new_text)
-elif arguments.type == 'decode':
-    if arguments.cipher == 'caesar':
-        if arguments.input_file != None:
-            text = take_out_of_file(arguments.input_file)
-        else:
-            text = str(input())
+    elif arguments.cipher == 'caesar' and arguments.mode == 'decode':
         new_text = caesar(text, alphabet, -int(arguments.key))
-        if arguments.output_file != None:
-            put_into_file(arguments.output_file, new_text)
-        else:
-            print(new_text)
-    elif arguments.cipher == 'vigenere':
-        if arguments.input_file != None:
-            text = take_out_of_file(arguments.input_file)
-        else:
-            text = str(input())
-        reversed_key = list()
-        for i in range(len(arguments.key)):
-           reversed_key.append(alphabet[(len(alphabet) - alphabet.index(arguments.key[i])) % len(alphabet)])
-        reversed_key = ''.join(reversed_key)
-        new_text = vigenere(text, alphabet, reversed_key)
-        if arguments.output_file != None:
-            put_into_file(arguments.output_file, new_text)
-        else:
-            print(new_text)
-elif arguments.type == 'train':
+    elif arguments.cipher == 'vigenere' and arguments.mode == 'encode':
+        new_text = vigenere(text, alphabet, arguments.key)
+    elif arguments.cipher == 'vigenere' and arguments.mode == 'decode':
+        new_key = reversed_key(alphabet, arguments.key)
+        new_text = vigenere(text, alphabet, new_key)
+    if arguments.output_file != None:
+        put_into_file(arguments.output_file, new_text)
+    else:
+        print(new_text)
+elif arguments.mode == 'train':
     if(arguments.text_file != None):
         text = take_out_of_file(arguments.text_file)
     else:
         text = str(input())
     info = statistic(text, alphabet_lower)
     put_into_file(arguments.model_file, ' '.join(info))
-elif arguments.type == 'hack':
+elif arguments.mode == 'hack':
     if arguments.input_file != None:
         text = take_out_of_file(arguments.input_file)
     else:
